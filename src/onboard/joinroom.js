5bb93fb404c07a63305b8cffba31125b86c52900
Jose-cami <201248@ids.upchiapas.edu.mx>
2021-07-27 00:23:32 -0500

    make it better

--- src/onboard/joinroom.js
+++ src/onboard/joinroom.js
@@ -55,7 +55,7 @@ class JoinRoom extends React.Component {
                             this.setState({
                                 didGetUserName: true
                             })
-                        }}>Submit</button>
+                        }}>Iniciar</button>
                 </div>
             }
             </React.Fragment>)
